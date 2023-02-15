6042fb09f06a268427db03908b4537bed5de0607
Zach Lawrence <zclawrence@ucsd.edu>
2023-02-15 14:39:42 -0800

    completed grade bash script, WIP filter test methods

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,47 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test
+  public void testFilterEmpty(){
+    List<String> empty = Arrays.asList();
+    List<String> result = ListExamples.filter(empty, new IsMoon());
+    List<String> expected = Arrays.asList();
+    assertEquals(expected, result);
+    assertEquals(0, result.size());
+  }
+
+  @Test
+  public void testFilterNoMatches(){
+    List<String> full = Arrays.asList("a", "b", "c");
+    List<String> expected = Arrays.asList();
+    List<String> result = ListExamples.filter(full, new IsMoon());
+    assertEquals(expected, result);
+    assertEquals(0, result.size());
+  }
+
+  
+  @Test
+  public void testFilterAllMatches(){
+    List<String> full = Arrays.asList("moon", "moon", "moon");
+    List<String> expected = Arrays.asList("moon", "moon", "moon");
+    List<String> result = ListExamples.filter(full, new IsMoon());
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void testFilterSomeMatches(){
+    List<String> full = Arrays.asList("moon", "moon", "sun");
+    List<String> expected = Arrays.asList("moon", "moon");
+    List<String> result = ListExamples.filter(full, new IsMoon());
+    assertEquals(expected, result);
+    full = Arrays.asList("sun", "moon", "sun");
+    expected = Arrays.asList("moon");
+    result = ListExamples.filter(full, new IsMoon());
+    assertEquals(expected,  result);
+    full = Arrays.asList("moon", "moon", "sun", "moon");
+    expected = Arrays.asList("moon", "moon", "moon");
+    result = ListExamples.filter(full, new IsMoon());
+    assertEquals(expected, result);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,5 +1,32 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [ -f student-submission/ListExamples.java ]
+then 
+    echo 'Java file found'
+    javac student-submission/ListExamples.java
+else
+    echo 'Error: Java file not found, error code '$?''
+    exit
+fi
+
+for file in *.java
+do
+    cp $file student-submission/$file 
+done
+
+cd student-submission
+javac -cp $CPATH *.java
+if [ $? -eq 0 ]
+then 
+    echo 'Compiled successfully'
+else
+    echo 'Compile error code '$?''
+    exit
+fi
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > output.txt 2>&1
+cat output.txt
